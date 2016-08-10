3dd7e738b98d3e0ba52930b189a9b28d53e44230
Tony Blake <avblake225@gmail.com>
2016-08-10 13:30:48 +0100

    Returned app to working state

--- app/src/main/java/com/tonyblake/songmojo/MainActivity.java
+++ app/src/main/java/com/tonyblake/songmojo/MainActivity.java
@@ -4,15 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-
 public class MainActivity extends AppCompatActivity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String FCMRegistrationToken = FirebaseInstanceId.getInstance().getToken();
 
         Intent intent = new Intent(this, Login.class);
 
