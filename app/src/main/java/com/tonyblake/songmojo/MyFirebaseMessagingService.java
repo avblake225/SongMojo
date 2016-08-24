243c0e77b86bcdde3e216ace9ef343369c6efead
Tony Blake <avblake225@gmail.com>
2016-08-24 10:58:42 +0100

    Successfully sending test notification from FCM backend to client app

--- app/src/main/java/com/tonyblake/songmojo/MyFirebaseMessagingService.java
+++ app/src/main/java/com/tonyblake/songmojo/MyFirebaseMessagingService.java
@@ -67,7 +67,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                //.setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setSmallIcon(R.drawable.songmojo_logo)
                 .setContentTitle("FCM Message")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
